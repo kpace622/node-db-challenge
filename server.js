4e007406d77bea862ccbc3579dbe12a825b811c1
Kyle Pace <kpace2296@gmail.com>
2020-04-17 21:33:58 -0500

    almost done

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const ProjectRouter = require('./project/project-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/projects', ProjectRouter);
+
+module.exports = server;
